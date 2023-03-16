8dd451edcf7bc89087aa9f3981758447595b5834
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-16 16:12:34 +0100

    add secondo exercise !!!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,38 @@ function palWord() {
     const value = palWord();  
   
     console.log(value); 
+
+
+
+
+//Pari e Dispari
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+const numberPlayer = Number( prompt('inserisci un numero da 1 a 5'))
+console.log(numberPlayer);
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+const numberComputer = Math.floor(Math.random() * 5) + 1;
+console.log(numberComputer);
+//Sommiamo i due numeri 
+
+const totalNumber = numberPlayer + numberComputer
+console.log(totalNumber);
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+if (totalNumber % 2 === 0) {
+    alert('il numero è pari')
+} else {
+
+    alert('il numero è dispari')
+}
+
+//Dichiariamo chi ha vinto.
+
+if ((totalNumber % 2 === 0) === true) {
+
+    alert('il giocatore ha vinto')
+    
+} else {
+    
+    alert('il giocatore ha perso')
+}

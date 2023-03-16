3912d00e3f7f041bf733068515a7015fd6a58871
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-16 15:58:54 +0100

    add first part exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 //Palidroma
-//Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-/* let i = 0
 
-for (let i = 0; i < number.length; i++) {
+//Chiedere all’utente di inserire una parola 
+const word = prompt('inserisci una parola');
+console.log(word);
 
-    const word = Number( prompt('inserisci un numero'));
-    const element = word[i];
-    console.log(element);
+//Creare una funzione per capire se la parola inserita è palindroma
+
+function palWord() {
     
-} */
+    for (let i = 0; i < word.length / 2; i++) {
+
+         if (word[i] !== word[word.length - 1 - i]) {  
+                alert( 'It is not a palindrome');  
+            }  else { 
+                alert( 'It is a palindrome');  
+                } 
+        } 
+           
+    }
+
+    const value = palWord();  
+  
+    console.log(value); 

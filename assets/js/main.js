e32bdfa11c0d8e7e64490dc9006c868b52aa5fe2
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-16 15:29:35 +0100

    start exercise

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//Palidroma
+//Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+/* let i = 0
+
+for (let i = 0; i < number.length; i++) {
+
+    const word = Number( prompt('inserisci un numero'));
+    const element = word[i];
+    console.log(element);
+    
+} */
